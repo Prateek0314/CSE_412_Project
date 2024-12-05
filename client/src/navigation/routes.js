4a4108e8be10c10158aeaf4b273c1c2bbf5548c8
rdanti <rdanti@asu.edu>
2024-12-04 19:39:01 -0700

    More setup + db dump

---
+++ client/src/navigation/routes.js
@@ -0,0 +1,7 @@
+export const Paths = {
+    ADMIN: '/admin',
+    SHOP: '/shop',
+    ITEM: '/item',
+    CHECKOUT: '/checkout',
+    DEFAULT: '/'
+}
---
+++ client/src/theme/color.js
@@ -0,0 +1,17 @@
+import { palette } from "./palette";
+
+export const color = {
+    // Theme colors
+    primary: palette.hookersGreen,
+    secondary: palette.tiffanyBlue,
+    tertiaryText: palette.frenchGray,
+
+    // Greys
+    bodyText: palette.black,
+    offWhite: palette.offWhite,
+    background: palette.azure,
+
+    // Semantics
+    success: palette.green,
+    error: palette.red
+}
