df669a2d41579376c8523a50fab3bd3c9f4b96aa
Prateek Ravindran <prateeksr2003@gmail.com>
2024-12-05 20:02:57 -0700

    Update function created
    Added sql function to prevent sql injections, Added Update transaction function

--- server/main.py
+++ server/main.py
@@ -1,5 +1,6 @@
 from flask import Flask, jsonify, request
 import psycopg2
+from psycopg2 import sql
 from psycopg2.extras import RealDictCursor
 import os
 from flask_cors import CORS
@@ -98,12 +99,17 @@ def get_item_info():
 @app.route('/customer', methods=['POST'])
 def add_customer():
     try:
+        #Getting customer info
         customer_data = request.json
+        
+        #Fields that are necessary to be filled for a customer to be added
         required_fields = ['Customer_ID', 'Gender', 'Location', 'Total_Spent', 'Tenure_Months']
+
+        #Validating that all required fields are present in the data payload
         if not all(field in customer_data for field in required_fields):
             return jsonify({"error": "Missing required fields"}), 400
         
-        conn = establish_connection()
+        conn = establishConnection()
         cursor = conn.cursor()
         cursor.execute(''' INSERT INTO Customer_Info (Customer_ID, Gender, Location, Total_Spent, Tenure_Months) VALUES (%s, %s, %s, %s, %s)''',
             (
@@ -121,6 +127,36 @@ def add_customer():
     except Exception as e:
         return jsonify({'error': str(e)}), 500
 
+@app.route('/transaction', methods=['PUT'])
+def update_transaction():
+    try:
+        transaction_data = request.json
+        
+        #Extracting transaction info and updates from payload
+        transaction_id = transaction_data.get('Transaction_ID')
+        updates = transaction_data.get('updates', {})
+        
+        if not transaction_id or not updates:
+            return jsonify({"error": "Transaction_ID and updates are required"}), 400
+        
+        conn = establishConnection()
+        cursor = conn.cursor()
+
+        #Iterating through dictionary of updates
+        for column, value in updates.items():
+
+            #Dynamic query construction
+            cursor.execute(
+                sql.SQL("UPDATE Transaction_Info SET {} = %s WHERE Transaction_ID = %s")
+                .format(sql.Identifier(column)), #Safely handle column names to prevent SQL injection
+                (value, transaction_id)  #Setting the new values
+            )
+        conn.commit()
+        cursor.close()
+        conn.close()
+        return jsonify({"message": "Transaction updated successfully"}), 200  #Success Response
+    except Exception as e:
+        return jsonify({'error': str(e)}), 500              #Error response
 
 if __name__ == '__main__':
     app.run(debug=True)
